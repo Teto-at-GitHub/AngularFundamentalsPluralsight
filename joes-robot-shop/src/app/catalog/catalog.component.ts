2af6f1873d9c4804198ecba9db95278f957c6114
SC <>
2025-04-11 15:59:12 +0200

    feat: completes using angular component syntax

---
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-catalog',
+  standalone: false,
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css'
+})
+export class CatalogComponent {
+
+}
---
+++ joes-robot-shop/src/app/site-header/site-header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css'],
+  standalone: false
+})
+
+export class SiteHeaderComponent {
+
+  constructor() { }
+
+}
